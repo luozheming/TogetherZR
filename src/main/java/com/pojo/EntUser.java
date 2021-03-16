b8a5eaf902e7feb87f09639ecaee7a991f3d2ea6
XIAO_明 <lzm931105@163.com>
2021-03-16 12:02:40 +0800

    #comment 调整部分代码

--- src/main/java/com/pojo/EntUser.java
+++ src/main/java/com/pojo/EntUser.java
@@ -1,8 +1,54 @@
 package com.pojo;
 
 
+import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
 
 @Data
+@Builder
+@Document(collection = "entuser")
 public class EntUser {
+    /**
+     *用户唯一识别键
+     */
+    @Id
+    private String openId;
+    /**
+     *手机号码
+     */
+    private String phoneNm;
+    /**
+     *用户微信昵称
+     */
+    private String nickName;
+    /**
+     * 性别
+     */
+    private String gender;
+    /**
+     * 城市
+     */
+    private String city;
+    /**
+     * 省
+     */
+    private String province;
+    /**
+     * 国家
+     */
+    private String country;
+    /**
+     * 项目
+     */
+    private List<Project> projects;
+
+    @Data
+    public class Project {
+        private String projectNo;
+        private String projectNm;
+    }
 }
--- src/main/java/com/pojo/Investor.java
+++ src/main/java/com/pojo/Investor.java
@@ -3,9 +3,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.Tolerate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Builder
+@Document(value = "investor")
 public class Investor {
     /**
      * 投资人 Id
